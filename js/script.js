a44ec5c70654b5c05b5aa16fa8c9276f8f1ded3e
rjsander <rjsander19@gmail.com>
2023-03-20 12:01:24 -0700

    functioning api and styling

--- js/script.js
+++ js/script.js
@@ -3,16 +3,9 @@ let countryData, userInput;
 const $name = $('#name')
 const $capital = $('#capital')
 const $continents = $('#continents')
-const $languages = $('#languages')
 const $flag = $('#flag')
 const $population = $('#population')
-
-
-
-
-
-
-
+const $languages = $('#languages')
 
 const $input = $('input[type="text"]')
 
@@ -24,7 +17,6 @@ function handleGetData(event) {
     userInput = $input.val();
 $.ajax({
   url: `https://restcountries.com/v3.1/name/${userInput}?fullText=true`
-
 }).then(
   (data) => {
    countryData = data[0];
@@ -41,7 +33,13 @@ function render() {
     $name.text(countryData.name.common)
     $capital.text(countryData.capital)
     $continents.text(countryData.continents)
-    $languages.text(countryData.languages.eng)
     $flag.text(countryData.flag)
+    $languages.text(countryData.languages)
     $population.text(countryData.population)
 }
+
+
+// to make languages populate:
+// 1. Have a dom element created on the html page ready to receive those newly found languages.
+// 2. Grab every key or value in the language object(it’s up to you)
+// 3. Use a method on the result of the above step  to append each key to the dom element created on the html page
